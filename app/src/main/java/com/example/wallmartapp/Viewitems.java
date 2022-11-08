309defc31fbc3b38784d5d9d6247b5f2d1b94528
codeCrackerAbi <abinashrijal42@gmail.com>
2022-11-08 10:07:36 +0545

    WallMart-App

---
+++ app/src/main/java/com/example/wallmartapp/Viewitems.java
@@ -0,0 +1,14 @@
+package com.example.wallmartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Viewitems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewitems);
+    }
+}
